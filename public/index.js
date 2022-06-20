8f6d7d1f28ee6028add066a9bc0af0e673bbf2ed
headbwoi <tochukwujohn24@gmail.com>
2022-06-20 01:03:03 +0100

    edited cta btn and wrote code for dropdown toggle

--- public/index.js
+++ public/index.js
@@ -1,4 +1,35 @@
-const dropLinks = document.querySelectorAll(".drop-link")
 const dropDownMenu = document.querySelectorAll(".dropmenu") 
+const dropLink1 = document.querySelector(".drop-link")
+const dropLink2 = document.querySelector(".drop-link2")
+const dropLink3 = document.querySelector(".drop-link3")
+const dropDownMenu1 = document.querySelector(".dropmenu1") 
+const dropDownMenu2 = document.querySelector(".dropmenu2") 
+const dropDownMenu3 = document.querySelector(".dropmenu3") 
 
+const removeActiveClass = () => {
+    dropDownMenu.forEach(item => {
+        item.classList.remove("active")
+    })
+}
 
+dropLink1.addEventListener("click", () => {
+    removeActiveClass()
+    dropDownMenu1.classList.add("active")
+    dropLink1.addEventListener("click", () => {
+        dropDownMenu1.classList.contains("active")? dropDownMenu1.classList.remove("active") : dropDownMenu1.classList.add("active")
+    })
+})
+dropLink2.addEventListener("click", () => {
+    removeActiveClass()
+    dropDownMenu2.classList.add("active")
+    dropLink2.addEventListener("click", () => {
+        dropDownMenu2.classList.contains("active")? dropDownMenu2.classList.remove("active") : dropDownMenu2.classList.add("active")
+    })
+})
+dropLink3.addEventListener("click", () => {
+    removeActiveClass()
+    dropDownMenu3.classList.add("active")
+    dropLink3.addEventListener("click", () => {
+        dropDownMenu3.classList.contains("active")? dropDownMenu3.classList.remove("active") : dropDownMenu3.classList.add("active")
+    })
+})
