c1a4def0f089a463050bbe9cdaf15a7792df156e
headbwoi <tochukwujohn24@gmail.com>
2022-06-22 22:04:13 +0100

    finally finished the annoying navigation section

--- public/index.js
+++ public/index.js
@@ -34,6 +34,46 @@ dropLink3.addEventListener("click", () => {
     })
 })
 
-const mobileDRopdown = document.querySelectorAll(".mobiledropdown")
+
+//mobmenu toggle
+const mobileMenu = document.querySelector(".mobilemenu")
+const hamburgerIcon = document.querySelector("#hamburger")
+
+hamburgerIcon.addEventListener("click", () => {
+    mobileMenu.classList.toggle("show")
+    if(mobileMenu.classList.contains("show")){
+        hamburgerIcon.setAttribute("src", "images/icon-close.svg")
+    }else{        
+        hamburgerIcon.setAttribute("src", "images/icon-hamburger.svg")
+}
+})
+
+
+const mobileDropdown = document.querySelectorAll(".mobiledropdown")
 const openLink = document.querySelectorAll(".open-link")
+const mobiledropdown1 = document.querySelector(".mobiledropdown1")
+const mobiledropdown2 = document.querySelector(".mobiledropdown2")
+const mobiledropdown3 = document.querySelector(".mobiledropdown3")
+
+const addHiddenClass = () => {
+    openLink.forEach(link => {
+        link.classList.add("hidden")
+    })
+}
+
+mobiledropdown1.addEventListener("click", () => {
+    const openLink = document.querySelector(".open-link1")
+    addHiddenClass()
+    openLink.classList.toggle("hidden")
+})
+mobiledropdown2.addEventListener("click", () => {
+    const openLink = document.querySelector(".open-link2")
+    addHiddenClass()
+    openLink.classList.toggle("hidden")
+})
+mobiledropdown3.addEventListener("click", () => {
+    const openLink = document.querySelector(".open-link3")
+    addHiddenClass()
+    openLink.classList.toggle("hidden")
+})
 
